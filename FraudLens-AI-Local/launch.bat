95abadee165cb96903425d340f8561065b12a8f4
edgarwrightiv <edgarwrightiv@gmail.com>
2026-02-14 12:26:01 -0700

    Delete FraudLens-AI-Local directory
    Remove nested folder

--- FraudLens-AI-Local/launch.bat
+++
@@ -1,5 +0,0 @@
-@echo off
-title Launch Backend
-start cmd /k "cd /d \"C:\Users\edgar\OneDrive\Desktop\FraudLens-AI\backend\" && uvicorn main:app --reload"
-title Launch Frontend
-start cmd /k "cd /d \"C:\Users\edgar\OneDrive\Desktop\FraudLens-AI\frontend\" && npm start"
